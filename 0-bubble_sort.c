7c9b66599e8b687aba53800705c10532cb043894
Iglcyril <iglcyril@hotmail.fr>
2025-12-10 13:38:29 +0000

    fix: betty error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,14 +13,14 @@ void bubble_sort(int *array, size_t size)
 	size_t temp;
 	size_t n = size;
 
-	for (i=0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j=0; j < n-1; j++)
+		for (j = 0; j < n - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j +1 ])
 			{
-				temp = array[j+1];
-				array[j+1] = array[j];
+				temp = array[j +1 ];
+				array[j + 1] = array[j];
 				array[j] = temp;
 				print_array(array, size);
 			}

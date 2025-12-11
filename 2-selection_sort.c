a57ecdab4155ab573cd8ddc8ce3dcf48a4626662
Iglcyril <iglcyril@hotmail.fr>
2025-12-11 12:12:33 +0000

    feat: create a function that sorts an array of integers in ascending order using the Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ *                  using the Selection sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	size_t temp;
+	int numsmall;
+	size_t index_min;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		numsmall = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < numsmall)
+			{
+				numsmall = array[j];
+				index_min = j;
+			}
+		}
+			temp = array[i];
+			array[i] = array[index_min];
+			array[index_min] = temp;
+
+		if(i != index_min)
+			print_array(array, size);
+	}
+	
+}
+
